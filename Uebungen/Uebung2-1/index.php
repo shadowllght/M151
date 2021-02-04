f75f61d69db34a32b617084a9cd038ba94ff004a
shadowllght <noahkazuki_goetschi@sluz.ch>
2021-02-04 09:24:23 +0100

    Ü2-1

---
+++ Uebungen/Uebung2-1/index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "vmadmin";
+$password = "sml12345";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully\n";
+
+//mysqli_select_db($conn, dbname, 'northwind');
+
+$sql = "SELECT * FROM northwind.customers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  echo $result->num_rows . "Resultate";
+} else {
+  echo "Keine Resultate vorhanden";
+}
+
+//var_dump($result);
+
+mysqli_connect_error($conn);
+?>
