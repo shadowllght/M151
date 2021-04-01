17398a0b6d2604a3422ed37b7d98f25a73420bde
vmadmin <vmadmin@bmLP1.smartlearn.wan>
2021-04-01 10:23:57 +0200

    idk at this point

--- laravel/app/Http/Controllers/OrderItemController.php
+++ laravel/app/Http/Controllers/OrderItemController.php
@@ -2,12 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
-    public function addProduct($id, Request $request)
+    public function Cart()
     {
-        $this->$request;
+        return;
+    }
+    public function addToCart($id)
+    {
+        $product = Order::find($id);
+
+        if (!$product) {
+
+            abort(404);
+        }
+
+        $cart = session()->get('order');
+        if (!$cart) {
+
+            $cart = [
+                $id => [
+                    "name" => $product->name,
+                    "quantity" => 1,
+                    "price" => $product->price,
+                    "details" => $product->details,
+                    "photo" => $product->image,
+
+                ]
+            ];
+
+            session()->put('cart', $cart);
+
+            return redirect()->back()->with('Product added');
+        }
+
+        if (isset($cart[$id])) {
+
+            $cart[$id]['quantity']++;
+
+            session()->put('cart', $cart);
+
+            return redirect()->back()->with('Product added');
+        }
+
+        $cart[$id] = [
+            "name" => $product->name,
+            "quantity" => 1,
+            "price" => $product->price,
+            "details" => $product->details,
+            "photo" => $product->image,
+        ];
+
+        session()->put('cart', $cart);
+
+        return redirect()->back()->with('Product added');
     }
 }
